6beb7d5b37ace98cb2f3997ddcce1f0ed18f6a40
MingfeiCheng <snowbirds.mf@gmail.com>
2026-04-15 16:41:46 +0800

    update readme

--- install_all.sh
+++ install_all.sh
@@ -7,16 +7,16 @@ VENV_ROOT=".venvs"
 mkdir -p "$VENV_ROOT"
 
 # ADS agents
-bash install_ads_eval.sh roach        "$CARLA_VERSION" "$VENV_ROOT/roach" # ok
-bash install_ads_eval.sh interfuser   "$CARLA_VERSION" "$VENV_ROOT/interfuser" # ok
-bash install_ads_eval.sh lav          "$CARLA_VERSION" "$VENV_ROOT/lav" # ok
-bash install_ads_eval.sh orion        "$CARLA_VERSION" "$VENV_ROOT/orion" # can install but limited by GPU memory
-bash install_ads_eval.sh plant        "$CARLA_VERSION" "$VENV_ROOT/plant" # ok
-bash install_ads_eval.sh simlingo     "$CARLA_VERSION" "$VENV_ROOT/simlingo" # ok
-bash install_ads_eval.sh tcp_admlp    "$CARLA_VERSION" "$VENV_ROOT/tcp_admlp" # tcp ok, admlp -> not move 
-bash install_ads_eval.sh transfuser   "$CARLA_VERSION" "$VENV_ROOT/transfuser" # ok
-bash install_ads_eval.sh uniad_vad    "$CARLA_VERSION" "$VENV_ROOT/uniad_vad" # uniad ok, vad -> ok
-bash install_ads_eval.sh pylot    "$CARLA_VERSION" "$VENV_ROOT/pylot" # ok
+bash install_ads_eval.sh roach        "$CARLA_VERSION" "$VENV_ROOT/roach" 
+bash install_ads_eval.sh interfuser   "$CARLA_VERSION" "$VENV_ROOT/interfuser" 
+bash install_ads_eval.sh lav          "$CARLA_VERSION" "$VENV_ROOT/lav" 
+bash install_ads_eval.sh orion        "$CARLA_VERSION" "$VENV_ROOT/orion" 
+bash install_ads_eval.sh plant        "$CARLA_VERSION" "$VENV_ROOT/plant" 
+bash install_ads_eval.sh simlingo     "$CARLA_VERSION" "$VENV_ROOT/simlingo" 
+bash install_ads_eval.sh tcp_admlp    "$CARLA_VERSION" "$VENV_ROOT/tcp_admlp" # tcp & admlp
+bash install_ads_eval.sh transfuser   "$CARLA_VERSION" "$VENV_ROOT/transfuser" 
+bash install_ads_eval.sh uniad_vad    "$CARLA_VERSION" "$VENV_ROOT/uniad_vad" # uniad & vad
+bash install_ads_eval.sh pylot    "$CARLA_VERSION" "$VENV_ROOT/pylot"
 
 
 # Testers
